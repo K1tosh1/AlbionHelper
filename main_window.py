e1e077a45f816a40d34250f1acdb296a4142533a
K1tosh1 <127305175+K1tosh1@users.noreply.github.com>
2023-05-30 04:28:27 +0300

    update

--- main_window.py
+++ main_window.py
@@ -1,11 +1,13 @@
 import customtkinter
 from CTkMessagebox import CTkMessagebox
+import requests
 
 class Albion_Helper(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         customtkinter.set_appearance_mode("dark")
         self.title("Albion Helper")
+        self.selected_server = None
 
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
@@ -23,15 +25,15 @@ class Albion_Helper(customtkinter.CTk):
         sidebar = customtkinter.CTkFrame(self, width=sidebar_width, fg_color="#222831")
         sidebar.grid(row=0, column=0, sticky="ns")
 
-        category = customtkinter.CTkComboBox(sidebar, values=['Сумки','Плащи','Тканевая броня','Тканевый шлем','Тканевая обувь','Кожаная броня','Кожаный шлем','Кожаная обувь','Латная броня','Латный шлем','Латная обувь'])
+        category = customtkinter.CTkComboBox(sidebar, values=['Сумки'], state = 'readonly',command=self.get_category)
         category.set(value="Выбери категорию")
         category.grid(row=0, column=0, pady=5,padx=5, sticky="nsew")
 
-        server = customtkinter.CTkComboBox(sidebar, values=['Западный','Восточный'])
+        server = customtkinter.CTkComboBox(sidebar, values=['Западный','Восточный'], state = 'readonly', command=self.get_server)
         server.set(value="Выбери сервер")
         server.grid(row=1, column=0, pady=5, padx=5,sticky="nsew")
 
-        premium = customtkinter.CTkComboBox(sidebar, values=['Да','Нет'])
+        premium = customtkinter.CTkComboBox(sidebar, values=['Да','Нет'], state = 'readonly', command=self.get_premium)
         premium.set(value="Премиум")
         premium.grid(row=2, column=0, pady=5, padx=5,sticky="nsew")
 
@@ -46,12 +48,37 @@ class Albion_Helper(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        self.show_info()
+    def get_server(self, value):
+        self.selected_server = value
+        if self.selected_server == "Западный":
+            url = "https://west.albion-online-data.com/api/v2/stats/prices/"
+            print(url)
+        elif self.selected_server == "Восточный":
+            url = "https://east.albion-online-data.com/api/v2/stats/prices/"
+            print(url)
+        return url
 
-    # FUNCTIONS AREA:
-    def show_info(self):
-        CTkMessagebox(title="Информация", message="Тут надо будет написать важную хуйню")
-        
+    def get_category(self, value):
+        self.selected_category = value
+        if self.selected_category == "Сумки":
+            item_ids = ['T2_BAG','T3_BAG','T4_BAG','T4_BAG@1','T4_BAG@2','T4_BAG@3','T4_BAG@4','T5_BAG','T5_BAG@1','T5_BAG@2','T5_BAG@3','T5_BAG@4',
+                        'T6_BAG','T6_BAG@1','T6_BAG@2','T6_BAG@3','T6_BAG@4','T7_BAG','T7_BAG@1','T7_BAG@2','T7_BAG@3','T7_BAG@4','T8_BAG','T8_BAG@1',
+                        'T8_BAG@2','T8_BAG@3','T8_BAG@4','T4_BAG_INSIGHT','T4_BAG_INSIGHT@1','T4_BAG_INSIGHT@2','T4_BAG_INSIGHT@3','T4_BAG_INSIGHT@4',
+                        'T5_BAG_INSIGHT','T5_BAG_INSIGHT@1','T5_BAG_INSIGHT@2','T5_BAG_INSIGHT@3','T5_BAG_INSIGHT@4','T6_BAG_INSIGHT','T6_BAG_INSIGHT@1',
+                        'T6_BAG_INSIGHT@2','T6_BAG_INSIGHT@3','T6_BAG_INSIGHT@4','T7_BAG_INSIGHT','T7_BAG_INSIGHT@1','T7_BAG_INSIGHT@2','T7_BAG_INSIGHT@3',
+                        'T7_BAG_INSIGHT@4','T8_BAG_INSIGHT','T8_BAG_INSIGHT@1','T8_BAG_INSIGHT@2','T8_BAG_INSIGHT@3','T8_BAG_INSIGHT@4']
+            print(item_ids)
+            return item_ids
+
+    def get_premium(self, value):
+        self.selected_premium = value
+        if self.selected_premium == "Да":
+            tax = 0.4
+            print(tax)
+        elif self.selected_premium == "Нет":
+            tax = 0.8
+            print(tax)
+        return tax
 
 
 if __name__ == "__main__":
