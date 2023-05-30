c75502f9cb124db32297787c3a7953c74b0d31a0
K1tosh1 <127305175+K1tosh1@users.noreply.github.com>
2023-05-30 03:55:06 +0300

    update

--- main_window.py
+++ main_window.py
@@ -1,9 +1,10 @@
 import customtkinter
+from CTkMessagebox import CTkMessagebox
 
 class Albion_Helper(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        customtkinter.set_appearance_mode("light")
+        customtkinter.set_appearance_mode("dark")
         self.title("Albion Helper")
 
         screen_width = self.winfo_screenwidth()
@@ -18,20 +19,39 @@ class Albion_Helper(customtkinter.CTk):
         self.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
         # Left sidebar
-        sidebar_width = 200
-        sidebar = customtkinter.CTkFrame(self, width=sidebar_width, fg_color="gray")
+        sidebar_width = 380
+        sidebar = customtkinter.CTkFrame(self, width=sidebar_width, fg_color="#222831")
         sidebar.grid(row=0, column=0, sticky="ns")
 
-        # Market button in the sidebar
-        market_button = customtkinter.CTkButton(sidebar, text="Market", fg_color="gray", border_width=1, border_color='lightgray')
-        market_button.pack(pady=10)
+        category = customtkinter.CTkComboBox(sidebar, values=['Сумки','Плащи','Тканевая броня','Тканевый шлем','Тканевая обувь','Кожаная броня','Кожаный шлем','Кожаная обувь','Латная броня','Латный шлем','Латная обувь'])
+        category.set(value="Выбери категорию")
+        category.grid(row=0, column=0, pady=5,padx=5, sticky="nsew")
+
+        server = customtkinter.CTkComboBox(sidebar, values=['Западный','Восточный'])
+        server.set(value="Выбери сервер")
+        server.grid(row=1, column=0, pady=5, padx=5,sticky="nsew")
+
+        premium = customtkinter.CTkComboBox(sidebar, values=['Да','Нет'])
+        premium.set(value="Премиум")
+        premium.grid(row=2, column=0, pady=5, padx=5,sticky="nsew")
+
+        btn_search = customtkinter.CTkButton(sidebar, text="Поиск")
+        btn_search.grid(row=3, column=0, padx=15, pady=15, sticky='nsew')
+
 
         # Main content area
-        content = customtkinter.CTkFrame(self, fg_color="white")
+        content = customtkinter.CTkScrollableFrame(self, fg_color="#393E46")
         content.grid(row=0, column=1, sticky="nsew")
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
+
+        self.show_info()
+
+    # FUNCTIONS AREA:
+    def show_info(self):
+        CTkMessagebox(title="Информация", message="Тут надо будет написать важную хуйню")
+        
 
 
 if __name__ == "__main__":
